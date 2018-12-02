65806e7fe77974def310be2edbbe0709f5e122d6
Nikolaj Understrup <nikolajgrue@gmail.com>
2018-12-02 19:58:02 +0100

    Add files via upload

--- make_data.py
+++ make_data.py
@@ -1,28 +1,81 @@
+# import numpy as np
+# import random
+from keras.utils import np_utils
+from matplotlib import pyplot as plt
+import h5py
+from PIL import ImageGrab, Image, ImageDraw
 import numpy as np
-import random
+import time
+import pprint as pp
+np.set_printoptions(threshold=np.nan)
 
-def make_colored_squares():
-	amount = 15000
-	height = 100
-	width = 100
-	rgb = 3
-	offset_x = 0
-	offset_y = 0
-	square_size = 5
+def generator_colored_squares():
+	while True:
+		amount = 100
+		height = 100
+		width = 100
+		rgb = 3
+		offset_x = 0
+		offset_y = 0
+		square_size = 10
 
-	image = np.random.rand(amount, height, width, rgb)
-	result = np.zeros(amount)
+		image = np.random.rand(amount, height, width, rgb)
+		result = np.zeros(amount)
 
-	for i in range(amount):
-		color = random.randint(0,2)
-		result[i] = color
-		offset_x = random.randint(0,95)
-		offset_y = random.randint(0,95)
+		for i in range(amount):
+			color = random.randint(0,2)
+			result[i] = color
+			offset_x = random.randint(0,width-square_size)
+			offset_y = random.randint(0,height-square_size)
 
-		for y in range(5):
-			for u in range(5):
-				image[i][offset_y:offset_y+square_size][y][offset_x:offset_x+square_size][u] = image[i][offset_y:offset_y+square_size][y][offset_x:offset_x+square_size][u] * 0.6
-				image[i][offset_y:offset_y+square_size][y][offset_x:offset_x+square_size][u][color] = 1.0
+			for y in range(square_size):
+				for u in range(square_size):
+					image[i][offset_y:offset_y+square_size][y][offset_x:offset_x+square_size][u] = image[i][offset_y:offset_y+square_size][y][offset_x:offset_x+square_size][u] * 0.75
+					image[i][offset_y:offset_y+square_size][y][offset_x:offset_x+square_size][u][color] = 1.0
 
-	return image[:int(amount*0.9)], result[:int(amount*0.9)], image[int(amount*0.9):], result[int(amount*0.9):]
+		result = np_utils.to_categorical(result, 3)
+		# print(result[10])
+		# plt.imshow(image[10])
+		# plt.show()
+		# exit()
+		yield image, result
+
+
+def generator_wow_hp_images(test_or_validation="training"):
+	f = h5py.File('wow-hp.hdf5', 'r')
+	start = 0
+	end = int( len(f)/2 )
+
+	if test_or_validation == "validation":
+		start = int(end*0.8)
+	elif test_or_validation == "training": 
+		end = int(end*0.8)
+
+	while True:
+		for i in range(start, end):
+			images = f.get("images{}".format(i)).value
+			results = np_utils.to_categorical(f.get("results{}".format(i)).value , 1215)
+			# print(results[4])
+			# plt.imshow(images[10])
+			# plt.show()
+			# exit()
+			yield images, results
+
+generator_wow_hp_images()
+
+def predict_wow_hp():
+	f = h5py.File('wow-hp.hdf5', 'r')
+	return f.get("images0").value[:1]
+
+
+# def predict_wow_hp():
+# 	images = []
+# 	for i in range(5):
+# 		imagegrab = ImageGrab.grab(bbox=(90,70,215,100))
+# 		pix = np.array(imagegrab)
+# 		images.append(pix)
+# 		time.sleep(0.1)
+# 	images = np.array(images)
+# 	# print(images.shape)
+# 	return images
 
--- test.py
+++ test.py
@@ -1,10 +1,5 @@
 from matplotlib import pyplot as plt
 import make_data
-X_train, y_train, X_test, y_test = make_data.make_colored_squares()
-# plt.imshow(X_train[5000])
-# plt.show()
-# exit()
-
 from keras.models import Sequential
 from keras.datasets import mnist
 from keras.layers import Dense, Dropout, Activation, Flatten
@@ -13,29 +8,37 @@ from keras.layers import *
 from keras.utils import np_utils
 import numpy as np
 
-Y_train = np_utils.to_categorical(y_train, 3)
-Y_test = np_utils.to_categorical(y_test, 3)
 
 model = Sequential()
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(100,100,3)))
-print (model.output_shape)
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(30,125,3)))
 
 model.add(Conv2D(32, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Dropout(0.25))
-print (model.output_shape)
-
+model.add(MaxPooling2D(pool_size=(3,3)))
+# model.add(Dropout(0.25))
 
 model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(3, activation='softmax'))
+model.add(Dense(1215, activation='relu'))
+# model.add(Dropout(0.5))
+model.add(Dense(1215, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-model.fit(X_train, Y_train, 
-          batch_size=300, epochs=5, verbose=1)
-score = model.evaluate(X_test, Y_test, verbose=0)
-print(score)
+
+#model lears to predict one specific integer, and nothing else. 
+prediction = model.predict_generator(make_data.generator_wow_hp_images("full"), steps=1, verbose=1) 
+for i in prediction:
+	print(np.argmax(i))
+
+exit()
+model.fit_generator(make_data.generator_wow_hp_images("full"), 
+	verbose=1, steps_per_epoch=500, epochs=1, validation_data=make_data.generator_wow_hp_images("full"), validation_steps=50, shuffle=True)
+prediction = model.predict_generator(make_data.generator_wow_hp_images("full"), steps=1, verbose=1) 
+# for i in prediction:
+# 	print(np.argmax(i))
+print(np.argmax(prediction[0]))
+print("length of predictions = {}".format(len(prediction)))
+
+# prediction = model.predict(make_data.predict_wow_hp(), batch_size=1, verbose=1)
+# print(prediction)
